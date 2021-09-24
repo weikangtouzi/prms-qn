3327dabe913ae1103f6c267ae202b92d0bf79457
卫正明 <1904628089@qq.com>
2021-09-24 22:06:10 +0800

    cssreset

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import '@/common/styles/reset.styl'
 import App from './App.vue'
 import router from './router'
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,16 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/feedback',
+    name: 'FeedBack',
+    component: () => import(/* webpackChunkName: "about" */ '../views/FeedBack.vue')
   }
 ]
 

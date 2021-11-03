2a326b102e81a436b234b7f0172e5ab51c2b4c83
卫正明 <weizhengming@xiaomi.com>
2021-11-03 15:16:36 +0800

    修改url

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import '@/common/stylus/reset.styl'
 // 与 API 的 HTTP 连接
 const httpLink = createHttpLink({
   // 你需要在这里使用绝对路径
-  uri: 'http://dev.chenzaozhao.com:4000/graphql'
+  uri: 'https://be.chenzaozhao.com/graphql'
 })
 
 const authLink = setContext((_, { headers }) => {
